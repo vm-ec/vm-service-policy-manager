d4a3a790fee92e7038f23a2d0a65f1c18ca59184
Ajay Yenugula <ajay.yenugula@valuemomentum.com>
2025-11-18 13:01:04 +0530

    first commit

---
+++ src/main/java/com/vm/service/policymanager/PolicymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.vm.service.policymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vm/service/policymanager/PolicymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.service.policymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
